9bbae92992d2d2f0054ff487c1c831ba52774ccc
Lucas Dias Noronha <lucasdiasnoronha1@gmail.com>
2024-06-26 16:42:35 -0300

    Second commit

--- regex.py
+++ regex.py
@@ -1,7 +1,6 @@
 import re
 
-texto = """
-Ao buscarem uma melhor compreensão e interpretação automatizada
+texto = """Ao buscarem uma melhor compreensão e interpretação automatizada
 de textos, as técnicas de processamento de linguagem natural (PLN) se
 deparam com o desafio de compreender variações em palavras com
 sentidos próximos, como no caso da língua portuguesa e de suas diversas
@@ -12,8 +11,7 @@ detectar padrões variados em textos.
 Neste capítulo, você identificará a importância das expressões regulares
 como recurso para o PLN, conhecerá os tipos de metacaracteres e
 implementará suas primeiros expressões regulares usando a linguagem
-de programação Python. 
-"""
+de programação Python"""
 
 # Metacaracteres representantes.
 padrão_busca1 = r"[^a-z]caract[a-z]{1,4}"
@@ -36,6 +34,30 @@ print("Resultado para padrão_busca4:", resultado4.group() if resultado4 else "N
 
 padrão_busca5 = r"expressõ[a-z]+"
 resultado5 = re.search(padrão_busca5, texto)
-print("Resultado para padrão_busca4:", resultado5.group() if resultado5 else "Nenhum resultado encontrado")
+print("Resultado para padrão_busca5:", resultado5.group() if resultado5 else "Nenhum resultado encontrado")
 
+# Metacaracteres Âncoras
+padrão_busca6 = r'^[Aa].*'
+resultado6 = re.search(padrão_busca6, texto)
+print("Resultado para padrão_busca6:", resultado6.group() if resultado6 else "Nenhum resultado encontrado")
+
+padrão_busca7 = r'[Pp][a-z]*$'
+resultado7 = re.search(padrão_busca7, texto)
+print("Resultado para padrão_busca7:", resultado7.group() if resultado7 else "Nenhum resultado encontrado")
+
+# Outros
+
+padrão_busca8 = r'expressões|regulares'
+resultado8 = re.search(padrão_busca8, texto)
+print("Resultado para padrão_busca8:", resultado8.group() if resultado8 else "Nenhum resultado encontrado")
+
+padrão_busca9 = r'(expressões|regulares)'
+resultado9 = re.findall(padrão_busca9, texto)
+print("Resultado para padrão_busca8:", resultado9 if resultado9 else "Nenhum resultado encontrado")
+
+texto2 = "Carros voando."
+
+padrão_busca10 = r'[Vv].*\.$'
+resultado10 = re.search(padrão_busca10, texto2)
+print("Resultado para padrão_busca10: ", resultado10.group() if resultado10 else "Nenhum resultado encontrado")
 
